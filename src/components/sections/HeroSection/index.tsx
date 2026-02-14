a25441cca528f5720101fa2bfb2b4ffe486a1c88
DioGenAsIs <neoboy773@gmail.com>
2026-02-15 03:09:33 +0500

    Update index.tsx

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -14,77 +14,98 @@ import Section from '../Section';
  The motivation for these annotations: allowing the content editor to edit styles at the field level.
  */
 export default function Component(props: HeroSection) {
-    const { elementId, colors, backgroundSize, title, subtitle, text, media, actions = [], styles = {} } = props;
-    const sectionFlexDirection = styles.self?.flexDirection ?? 'row';
-    const sectionAlign = styles.self?.textAlign ?? 'left';
-    return (
-        <Section elementId={elementId} colors={colors} backgroundSize={backgroundSize} styles={styles.self}>
-            <div className={classNames('flex gap-8', mapFlexDirectionStyles(sectionFlexDirection))}>
-                <div className={classNames('flex-1 w-full', mapStyles({ textAlign: sectionAlign }))}>
-                    {title && (
-                        <AnnotatedField path=".title">
-                            <h1 className="text-5xl sm:text-6xl">{title}</h1>
-                        </AnnotatedField>
-                    )}
-                    {subtitle && (
-                        <AnnotatedField path=".subtitle">
-                            <p className={classNames('text-xl sm:text-2xl', { 'mt-4': title })}>{subtitle}</p>
-                        </AnnotatedField>
-                    )}
-                    {text && (
-                        <AnnotatedField path=".text">
-                            <Markdown
-                                options={{ forceBlock: true, forceWrapper: true }}
-                                className={classNames('max-w-none prose sm:prose-lg', {
-                                    'mt-6': title || subtitle
-                                })}
-                            >
-                                {text}
-                            </Markdown>
-                        </AnnotatedField>
-                    )}
-                    {actions?.length > 0 && (
-                        <div
-                            className={classNames('flex flex-wrap items-center gap-4', {
-                                'mt-8': title || subtitle || text,
-                                'justify-center': sectionAlign === 'center',
-                                'justify-end': sectionAlign === 'right'
-                            })}
-                        >
-                            {actions.map((action, index) => (
-                                <Action key={index} {...action} />
-                            ))}
-                        </div>
-                    )}
-                </div>
-                {media && (
-                    <div
-                        className={classNames('flex flex-1 w-full', {
-                            'justify-center': sectionAlign === 'center',
-                            'justify-end': sectionAlign === 'right'
-                        })}
-                    >
-                        <HeroMedia media={media} />
-                    </div>
-                )}
+  const { elementId, colors, backgroundSize, title, subtitle, text, media, actions = [], styles = {} } = props;
+
+  const sectionFlexDirection = styles.self?.flexDirection ?? 'row';
+  const sectionAlign = styles.self?.textAlign ?? 'left';
+
+  return (
+    <Section elementId={elementId} colors={colors} backgroundSize={backgroundSize} styles={styles.self}>
+      <div className={classNames('flex gap-8', mapFlexDirectionStyles(sectionFlexDirection))}>
+        <div className={classNames('flex-1 w-full', mapStyles({ textAlign: sectionAlign }))}>
+          {title && (
+            <AnnotatedField path=".title">
+              <h1 className="text-5xl sm:text-6xl">{title}</h1>
+            </AnnotatedField>
+          )}
+
+          {subtitle && (
+            <AnnotatedField path=".subtitle">
+              <Markdown
+                options={{
+                  forceBlock: true,
+                  forceWrapper: true,
+                  overrides: {
+                    a: {
+                      props: {
+                        className: 'underline underline-offset-4 hover:opacity-80',
+                        target: '_blank',
+                        rel: 'noreferrer'
+                      }
+                    }
+                  }
+                }}
+                className={classNames('text-xl sm:text-2xl', { 'mt-4': !!title })}
+              >
+                {subtitle}
+              </Markdown>
+            </AnnotatedField>
+          )}
+
+          {text && (
+            <AnnotatedField path=".text">
+              <Markdown
+                options={{ forceBlock: true, forceWrapper: true }}
+                className={classNames('max-w-none prose sm:prose-lg', { 'mt-6': !!title || !!subtitle })}
+              >
+                {text}
+              </Markdown>
+            </AnnotatedField>
+          )}
+
+          {actions?.length > 0 && (
+            <div
+              className={classNames('flex flex-wrap items-center gap-4', {
+                'mt-8': !!title || !!subtitle || !!text,
+                'justify-center': sectionAlign === 'center',
+                'justify-end': sectionAlign === 'right'
+              })}
+            >
+              {actions.map((action, index) => (
+                <Action key={index} {...action} />
+              ))}
             </div>
-        </Section>
-    );
+          )}
+        </div>
+
+        {media && (
+          <div
+            className={classNames('flex flex-1 w-full', {
+              'justify-center': sectionAlign === 'center',
+              'justify-end': sectionAlign === 'right'
+            })}
+          >
+            <HeroMedia media={media} />
+          </div>
+        )}
+      </div>
+    </Section>
+  );
 }
 
 function HeroMedia({ media }) {
-    return <DynamicComponent {...media} />;
+  return <DynamicComponent {...media} />;
 }
 
 function mapFlexDirectionStyles(flexDirection?: 'row' | 'row-reverse' | 'col' | 'col-reverse') {
-    switch (flexDirection) {
-        case 'row-reverse':
-            return 'flex-col-reverse lg:flex-row-reverse lg:items-center';
-        case 'col':
-            return 'flex-col';
-        case 'col-reverse':
-            return 'flex-col-reverse';
-        default:
-            return 'flex-col lg:flex-row lg:items-center';
-    }
+  switch (flexDirection) {
+    case 'row-reverse':
+      return 'flex-col-reverse lg:flex-row-reverse lg:items-center';
+    case 'col':
+      return 'flex-col';
+    case 'col-reverse':
+      return 'flex-col-reverse';
+    default:
+      return 'flex-col lg:flex-row lg:items-center';
+  }
 }
